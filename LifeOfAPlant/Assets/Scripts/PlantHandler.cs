14b15721edc4b5ca2c4dad373e0e19781462dca9
Joel Hellberg <joehe737@student.liu.se>
2024-12-31 12:59:29 +0100

    Löven bytar nu färg

--- LifeOfAPlant/Assets/Scripts/PlantHandler.cs
+++ LifeOfAPlant/Assets/Scripts/PlantHandler.cs
@@ -9,13 +9,14 @@ using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using static UnityEditor.Progress;
 using UnityEditor.Experimental.GraphView;
 
-public class Spawner : MonoBehaviour
+public class PlantHandler : MonoBehaviour
 {
 
     public GameObject pot;
     public GameObject stemPrefab;
     public GameObject branchPrefab;
     public GameObject leafPrefab;
+    public Material leafMaterial;
 
     public UnityEngine.UI.Slider daysLivedSlider;
     public UnityEngine.UI.Slider waterSlider;
@@ -43,8 +44,40 @@ public class Spawner : MonoBehaviour
     void UpdateWaterStatus(float val)
     {
         float floatValue = waterSlider.value;
+        UpdateLeafColor(floatValue);
         ourPlant.updateRotation(floatValue);
         ourPlant.updatePlant();
     }
+
+    void UpdateLeafColor(float daysSinceWater)
+    {
+        float waterSliderMax = waterSlider.maxValue;
+
+        // Define the different colors of a leaf
+        Color greenColor = new Color(0, 128 / 255f, 0);
+        Color yellowColor = new Color(255 / 255f, 255 / 255f, 0);
+        Color brownColor = new Color(139 / 255f, 69 / 255f, 19 / 255f);
+
+        Color resultingColor;
+
+        if (daysSinceWater <= waterSliderMax / 2)
+        {
+            float halfway = waterSliderMax / 2;
+
+            resultingColor =
+                (greenColor * (halfway - daysSinceWater) / halfway) +
+                (yellowColor * daysSinceWater / halfway);
+        }
+        else
+        {
+            resultingColor =
+                (yellowColor * (waterSliderMax - daysSinceWater) / (waterSliderMax / 2)) +
+                (brownColor * (daysSinceWater - waterSliderMax / 2) / (waterSliderMax / 2));
+        }
+
+        // Update the color of the leafs material
+        leafMaterial.SetColor("_Color", resultingColor);
+    }
+
 
 }
